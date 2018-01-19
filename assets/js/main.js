b92addb2a3f5f982534c54a774f5a1a525ac2d47
Iñaki García Blasco <inaki@txorua.com>
2018-01-19 12:17:12 +0100

    Añado js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Is this browser sufficiently modern to continue?
+if ( !( "querySelector" in document && "addEventListener" in window && "getComputedStyle" in window) ) {
+  return;
+}
+window.document.documentElement.className += " enhanced";
