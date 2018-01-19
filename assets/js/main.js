92a0661a20abd4894f8726b34ade20850d26c8a6
Iñaki García Blasco <inaki@txorua.com>
2018-01-19 12:30:58 +0100

    Corrijo código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Is this browser sufficiently modern to continue?
 if ( !( "querySelector" in document && "addEventListener" in window && "getComputedStyle" in window) ) {
-  return;
+  () => {return;}
 }
 window.document.documentElement.className += " enhanced";
