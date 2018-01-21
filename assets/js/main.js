affccdbf5af7926565a4c97ca66174283e4bace4
Iñaki García Blasco <inaki@txorua.com>
2018-01-21 15:41:26 +0100

    Refactorizo script

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }, false)
   }
 
+  load()
+
 });
+
+function loadJSON(file, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+ }
+
+function load() {
+  loadJSON("/api/inmuebles.json", function(response) {
+    localSession.setItem("pisos", JSON.stringify(response))
+  });
+}
